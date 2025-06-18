ee171814a4b4d63c23bc665bdaaae30eb89207c8
Gung1s <tomas.strelciunas@gmail.com>
2025-06-18 16:38:03 +0300

    Funkcijos:visos lengvos užduotys

--- Lengvos_užduotys.py
+++ Lengvos_užduotys.py
@@ -141,3 +141,165 @@ cnt_smb("Šiandien labai graži diena")
 print()
 print("-------------------Vienuoliktas uždavinys-------------")
 print()
+
+# Sukurkite Funkciją kuri priimtų sakinį, jį užkoduotų ir grąžintų. Kodavimas - sakinį apsukame iš kitos pusės. Pvz “Naglis” turi gautis “silgaN”
+
+def rvrs(word):
+    return word[::-1]
+
+vardas = "Naglis"
+print(rvrs(vardas))
+print()
+
+print()
+print("-------------------Dvyliktas uždavinys-------------")
+print()
+
+# Sukurti funkciją, kuri apsuka tik žodžius. “Labas rytas” -> “sabal satyr”
+
+def rvrs_word(sentence):
+    words = sentence.split()
+    reversed_words = [word[::-1] for word in words]
+    return " ".join(reversed_words)
+
+sakinys = "Šiandien labai graži diena"
+print(rvrs_word(sakinys))
+
+print()
+print("-------------------Tryliktas uždavinys-------------")
+print()
+
+# Sukurkite funkciją, kuri priimtų masyvą ir atspausdintų tik tuos elementus kurie yra skaičiai.
+
+def print_nmb(array):
+    for i in array:
+        if isinstance(i, (int, float)):
+            print(i, end=" ")
+    print()
+
+print_nmb([1, "gražu", 5, 2.5])
+print()
+
+print()
+print("-------------------Keturioliktas uždavinys-------------")
+print()
+
+# Sukurkite funkciją, kuri iš paduoto masyvo atspausdina tik sveikuosius skaičius. (jei pavyks, patobulinkite,
+# kad funkcija priimtų antrą parametrą True/False kuris nuspręstų ar spausdins tik sveikuosius skaičius ar skaičius su kableliu.
+
+def print_nmb(array, integer = True):
+    for i in array:
+        if isinstance(i, (int, float)):
+            if integer and isinstance(i, int):
+                print(i, end=" ")
+            if not integer and isinstance(i, float):
+                print(i, end=" ")
+    print()
+
+print_nmb([1, "gražu", 5, 2.5], True)
+print()
+
+print()
+print("-------------------Penkioliktas uždavinys-------------")
+print()
+
+# Sukurkite funkciją word_count kuri priimtų textą ir gražintų kiek jame yra žodžių.
+
+def word_count(sentence):
+    words = sentence.split()
+    return len(words)
+
+sakinys = "Šiandien labai graži diena"
+print(word_count(sakinys))
+
+print()
+print("-------------------Šešioliktas uždavinys-------------")
+print()
+
+# Sukurkite funkciją kuri priima du parametrus. Skaičių masyvą ir boolean. Funkcija gražina prafiltruotą masyvą.
+# Kai antras parametras True/tik poriniais skaičiais, False/tik neporiniais skaičiais
+
+def even_nmb(array, even = True):
+    for i in array:
+        if even and i % 2 == 0:
+            print(i, end=" ")
+        if not even and i % 2 == 1:
+            print(i, end=" ")
+    print()
+
+masyvas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_nmb(masyvas, False)
+print()
+
+print()
+print("-------------------Septynioliktas uždavinys-------------")
+print()
+
+# Sukurkite funkciją number_is_prime. Funkcija priima skaičių, gražina True/False ar skaičius pirminis.
+
+def number_is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+number = 2
+print(number_is_prime(number))
+
+print()
+print("-------------------Aštuonioliktas uždavinys-------------")
+print()
+
+# Sukurkite funkciją kuri priima du argumentus. Gražina pirmąjį skaičių pakeltą laipsniu tokiu kaip antras skaičius.
+
+def sqr_nmb(x, y):
+    return print(x ** y)
+
+sqr_nmb(2, 3)
+
+print()
+print("-------------------Devynioliktas uždavinys-------------")
+print()
+
+# Sukurkite funkciją kuri priima skaičių masyvą ir gražina tik nepasikartojančius elementus
+
+def unique_nmb(array):
+    new_array = []
+    for i in array:
+        if i not in new_array:
+            new_array.append(i)
+    return new_array
+
+masyvas = [1, 2, 3, 1, 5, 9, 8, 7, 7]
+
+print(unique_nmb(masyvas), end=" ")
+print()
+
+print()
+print("-------------------Dvidešimtas uždavinys-------------")
+print()
+
+# Sukurkite funkciją kuri priima tekstą ir atspausdina tekste daugiausiai pasikartojantį simbolį.
+
+
+def cnt_smb(sentence):
+    return print(f"Daugiausiai pasikartojantis simbolis tekste: {max(sentence, key=sentence.count)}")
+
+sakinys = "Šiandien labai graži diena"
+cnt_smb(sakinys)
+
+print()
+print("-------------------Dvidešimt pirmas uždavinys-------------")
+print()
+
+# Sukurkite funkciją kuri priima tekstą ir atspausdina jame esantį ilgiausią žodį.
+
+def long_txt(sentence):
+    words = sentence.split()
+    return max(words, key=len)
+
+sakinys = "Šiandien labai graži diena"
+print(f"Ilgiausis žodis tekste: {long_txt(sakinys)}")
+print()
